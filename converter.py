6e344b91c7d308317524b326addcda9cf7e5338c
AndyinTheNW <andersonalmeida1008@gmail.com>
2023-06-29 19:03:49 -0300

    [Descrição resumida das alterações]
    Lista de alterações:
    - Removidos comentários e prints desnecessários
    - Adicionada importação do módulo `logging`
    - Implementado tratamento de erros mais detalhado com blocos `try-except`
    - Implementada função `verificar_arquivo_saida` para tratamento de nomes de arquivos de saída
    - Melhorada a legibilidade do código, incluindo espaçamentos adequados e formatação
    - Adicionadas anotações de tipo aos parâmetros e retornos das funções

--- converter.py
+++ converter.py
@@ -1,25 +1,69 @@
 import os
+import logging
 from pdfminer.high_level import extract_text
 
-def convert_pdf_to_txt(pdf_path, txt_dir):
-    filename = os.path.basename(pdf_path)
-    txt_filename = os.path.splitext(filename)[0] + ".txt"
-    txt_path = os.path.join(txt_dir, txt_filename)
-    text = extract_text(pdf_path)
-    with open(txt_path, "w", encoding="utf-8") as txt_file:
-        txt_file.write(text)
+EXTENSAO_TXT = ".txt"
 
-def convert_pdfs_in_folder(folder_path, output_folder):
-    pdf_files = [f for f in os.listdir(folder_path) if f.endswith(".pdf")]
-    for pdf_file in pdf_files:
-        pdf_path = os.path.join(folder_path, pdf_file)
-        convert_pdf_to_txt(pdf_path, output_folder)
+def converter_pdf_para_txt(caminho_pdf: str, pasta_txt: str) -> None:
+    try:
+        nome_arquivo = os.path.basename(caminho_pdf)
+        nome_arquivo_txt = os.path.splitext(nome_arquivo)[0] + EXTENSAO_TXT
+        caminho_txt = os.path.join(pasta_txt, nome_arquivo_txt)
 
-# Diretório contendo os arquivos PDF
-input_folder = "C:/epson_py/guias"
+        texto = extract_text(caminho_pdf)
 
-# Diretório de saída para os arquivos TXT
-output_folder = os.path.join(input_folder, "guias_txt")
-os.makedirs(output_folder, exist_ok=True)
+        with open(caminho_txt, "w", encoding="utf-8") as arquivo_txt:
+            arquivo_txt.write(texto)
 
-convert_pdfs_in_folder(input_folder, output_folder)
+        logging.info(f"Conversão concluída: {nome_arquivo}")
+    except Exception as e:
+        logging.error(f"Erro ao converter o arquivo {nome_arquivo}: {str(e)}")
+
+def converter_pdfs_na_pasta(caminho_pasta: str, pasta_saida: str) -> None:
+    try:
+        arquivos_pdf = [f for f in os.listdir(caminho_pasta) if f.endswith(".pdf")]
+
+        if not arquivos_pdf:
+            logging.warning("Nenhum arquivo PDF encontrado na pasta de entrada.")
+            return
+
+        for arquivo_pdf in arquivos_pdf:
+            caminho_pdf = os.path.join(caminho_pasta, arquivo_pdf)
+            converter_pdf_para_txt(caminho_pdf, pasta_saida)
+
+        logging.info("Conversão de PDFs concluída.")
+    except Exception as e:
+        logging.error(f"Erro ao converter os PDFs: {str(e)}")
+
+def criar_diretorio_saida(pasta_saida: str) -> None:
+    os.makedirs(pasta_saida, exist_ok=True)
+    logging.info(f"Diretório de saída criado: {pasta_saida}")
+
+def verificar_arquivo_saida(caminho_arquivo: str) -> str:
+    if os.path.exists(caminho_arquivo):
+        nome_arquivo, extensao_arquivo = os.path.splitext(caminho_arquivo)
+        contador = 1
+        novo_nome_arquivo = f"{nome_arquivo}_{contador}{extensao_arquivo}"
+        while os.path.exists(novo_nome_arquivo):
+            contador += 1
+            novo_nome_arquivo = f"{nome_arquivo}_{contador}{extensao_arquivo}"
+        return novo_nome_arquivo
+    return caminho_arquivo
+
+if __name__ == "__main__":
+    pasta_entrada: str = "C:\\conversor_extrator\\guias"
+    pasta_saida: str = os.path.join(pasta_entrada, "guias_txt")
+
+    logging.basicConfig(
+        filename="conversor.log",
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        encoding="utf-8"
+    )
+
+    try:
+        criar_diretorio_saida(pasta_saida)
+        converter_pdfs_na_pasta(pasta_entrada, pasta_saida)
+    except Exception as e:
+        logging.error(f"Erro ao processar os arquivos: {str(e)}")
